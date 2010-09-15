7e1d1dc94c126749096832703af62c3bdf5d840b
Fish <fish@scorpio.aqua>
2010-09-15 15:31:26 +0200

    Building error fix.

--- capabilities/spmlr2/command/src/main/java/org/atricore/idbus/capabilities/spmlr2/command/UserModifyCommand.java
+++ capabilities/spmlr2/command/src/main/java/org/atricore/idbus/capabilities/spmlr2/command/UserModifyCommand.java
@@ -78,7 +78,7 @@ public class UserModifyCommand extends SpmlCommandSupport {
 //<--- Security Set Password---->
     byte[] userCertificate;
     Boolean automaticallyGeneratePassword;
-1    Boolean emailNewPasword;
+    Boolean emailNewPasword;
 
 //<--- Groups Membership ---->
     @Option(name = "-g", aliases = "--group", description = "User group names", required = false, multiValued = true)
