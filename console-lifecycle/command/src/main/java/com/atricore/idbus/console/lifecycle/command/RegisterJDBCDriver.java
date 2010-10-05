13c4de7210f9382c3007a649e95be02dd115e0d3
sgonzalez <sgonzalez@atricore.com>
2010-10-05 15:54:30 -0300

    * FIX : Error when embedded resources were used

---
+++ console-lifecycle/command/src/main/java/com/atricore/idbus/console/lifecycle/command/RegisterJDBCDriver.java
@@ -0,0 +1,15 @@
+package com.atricore.idbus.console.lifecycle.command;
+
+import com.atricore.idbus.console.lifecycle.main.spi.IdentityApplianceManagementService;
+
+/**
+ * @author <a href=mailto:sgonzalez@atricor.org>Sebastian Gonzalez Oyuela</a>
+ */
+public class RegisterJDBCDriver extends ManagementCommandSupport {
+
+    @Override
+    protected Object doExecute(IdentityApplianceManagementService svc) throws Exception {
+        // TODO : Implement me
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
