1fa06351cdd27408928db423676f4e44f3daa930
Sebastian Gonzalez O <sgonzalez@atricore.com>
2010-12-15 14:37:40 -0300

    * Licensing (work in progress)

--- console-licensing/main/src/main/java/com/atricore/idbus/console/licensing/main/impl/LicenseManagerImpl.java
+++ console-licensing/main/src/main/java/com/atricore/idbus/console/licensing/main/impl/LicenseManagerImpl.java
@@ -11,19 +11,21 @@ import com.atricore.josso2.licensing._1_0.license.LicenseType;
 public class LicenseManagerImpl implements LicenseManager {
 
     // Enconded digital certifiate (in the future, we should be able to manage a set of certs).
-    private String licenseCertificate = "";
+    private String publicKey = "";
 
     public void activateLicense(byte[] license) throws InvalidLicenseException {
         // 1. Unmarshal and call validate again
     }
 
     public void activateLicense(LicenseType license) throws InvalidLicenseException {
-        // 1. Validate signature
-        // 2. Store license file in etc (DB in the future ?)
+        // 1. Validate license
+        validateLicense(license);
+        // 3. Store license file in etc (DB in the future ?)
     }
 
     public void validateLicense() throws InvalidLicenseException {
-
+        // 1. Retrieve license file
+        // 2. Validate
     }
 
     public void validateFeature(String group, String name) throws InvalidFeatureException {
@@ -34,7 +36,31 @@ public class LicenseManagerImpl implements LicenseManager {
         return null;
     }
 
-    protected void validateSignature(LicenseType license, String certificate) throws InvalidLicenseException {
+    protected void validateLicense(LicenseType license) throws InvalidLicenseException {
+        // 1. Validate signature
+        validateSignature(license, publicKey);
+
+        // 2. Validate license information
+        validateLicenseInformation(license);
 
     }
+
+    protected void validateSignature(LicenseType license, String certificate) throws InvalidLicenseException {
+        // TODO :
+    }
+
+    protected void validateLicenseInformation(LicenseType license) throws InvalidLicenseException {
+        // TODO : Validate expiration date, distribution type, etc.
+    }
+
+    protected LicenseType loadLicense() {
+        // Read license from disk for now (we could use DB in the future!)
+        // and unmarshall
+        return null;
+    }
+
+    protected void storeLicense(LicenseType license) {
+        // Marshall and write to disk
+    }
+
 }
