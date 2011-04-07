3bd4597e0f8edfaacf70cae03b75fd8752bd9dc1
Sebastian Gonzalez O <sgonzalez@atricore.com>
2011-04-07 12:03:45 -0300

    * 2 Factor Authn integration

---
+++ console-2factor/main/src/main/java/com/atricore/idbus/console/twofactor/spi/TwoFactorAuthenticator.java
@@ -0,0 +1,7 @@
+package com.atricore.idbus.console.twofactor.spi;
+
+/**
+ * @author <a href=mailto:sgonzalez@atricore.org>Sebastian Gonzalez Oyuela</a>
+ */
+public interface TwoFactorAuthenticator {
+}
