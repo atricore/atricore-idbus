9adae0c1bc070568475378e7971e42c49614fed5
Sebastian Gonzalez O <sgonzalez@atricore.com>
2011-04-13 13:48:41 -0400

    * Removed unused module

--- console-2factor/main/src/main/java/com/atricore/idbus/console/twofactor/spi/TwoFactorAuthenticator.java
+++
@@ -1,7 +0,0 @@
-package com.atricore.idbus.console.twofactor.spi;
-
-/**
- * @author <a href=mailto:sgonzalez@atricore.org>Sebastian Gonzalez Oyuela</a>
- */
-public interface TwoFactorAuthenticator {
-}
