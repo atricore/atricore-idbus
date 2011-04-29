c38ffe226dd3ee8344b52b6dd0153cdc56a60f65
Goran Nastov <kira@kiran.(none)>
2011-04-29 03:20:28 +0200

    [ATCON-280] LiveUpdate Service MBean (implemented getAvailableUpdates()).

--- console-liveservices/liveupdate/management/src/main/java/com/atricore/idbus/console/liveservices/liveupdate/management/LiveUpdateMBean.java
+++ console-liveservices/liveupdate/management/src/main/java/com/atricore/idbus/console/liveservices/liveupdate/management/LiveUpdateMBean.java
@@ -7,9 +7,22 @@ import javax.management.openmbean.TabularData;
  */
 public interface LiveUpdateMBean {
 
+    String UPDATE_DESCRIPTOR_NATURE = "Nature";
+
+    String UPDATE_DESCRIPTOR_GROUP = "Group";
+
+    String UPDATE_DESCRIPTOR_NAME = "Name";
+
+    String UPDATE_DESCRIPTOR_VERSION = "Version";
+
+    String UPDATE_DESCRIPTOR_DESCRIPTION = "Description";
+
+    String UPDATE_DESCRIPTOR_RELEASE_DATE = "Release date";
+
+    String[] UPDATE_DESCRIPTOR = { UPDATE_DESCRIPTOR_NATURE, UPDATE_DESCRIPTOR_GROUP, UPDATE_DESCRIPTOR_NAME,
+            UPDATE_DESCRIPTOR_VERSION, UPDATE_DESCRIPTOR_DESCRIPTION, UPDATE_DESCRIPTOR_RELEASE_DATE };
+
     TabularData getAvailableUpdates();
 
     TabularData getAvailableUpdates(String threshold);
-
-
 }
