7ab972f7d9c738b8514eb3782f38fb806a536f1d
Sebastian Gonzalez O <sgonzalez@atricore.com>
2011-05-04 17:15:54 -0300

    * JDO Bundle uses JDBC Driver manager

--- bundles/datanucleus-v2_1_2/bundle/src/main/java/org/atricore/idbus/bundles/datanucleus/core/OsgiJDOPersistenceManagerFactoryBean.java
+++ bundles/datanucleus-v2_1_2/bundle/src/main/java/org/atricore/idbus/bundles/datanucleus/core/OsgiJDOPersistenceManagerFactoryBean.java
@@ -5,11 +5,14 @@ import org.apache.commons.logging.LogFactory;
 import org.atricore.idbus.kernel.common.support.osgi.OsgiBundleClassLoader;
 import org.atricore.idbus.kernel.common.support.osgi.OsgiBundlespaceClassLoader;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.springframework.orm.jdo.LocalPersistenceManagerFactoryBean;
 import org.springframework.osgi.context.BundleContextAware;
 
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.util.Map;
 
 /**
@@ -20,9 +23,16 @@ public class OsgiJDOPersistenceManagerFactoryBean
         extends LocalPersistenceManagerFactoryBean
         implements BundleContextAware {
 
+    private static final String JNDI = "jndi:";
+    private static final String OSGI = "osgi:";
+
     private static final Log logger = LogFactory.getLog(OsgiJDOPersistenceManagerFactoryBean.class);
 
     private BundleContext bundleContext;
+
+    private String dataSourceUrl;
+
+    private DataSource dataSource;
 
     @Override
     protected PersistenceManagerFactory newPersistenceManagerFactory(Map props) {
@@ -43,6 +53,20 @@ public class OsgiJDOPersistenceManagerFactoryBean
                         bundleContext.getBundle().getLocation());
             props.put("datanucleus.primaryClassLoader", osgiCl);
 
+        }
+        try {
+            if (dataSource == null) {
+                if (dataSourceUrl == null)
+                    dataSourceUrl = (String) props.get("jdbc.dataSourceUrl");
+
+                dataSource = (DataSource) createDatasource(dataSourceUrl);
+
+            }
+
+            props.put("datanucleus.ConnectionFactory", dataSource);
+
+        } catch (Exception e) {
+            logger.error("Cannot locate DS for " + dataSourceUrl, e);
         }
 
         PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory(props, osgiCl);
@@ -78,5 +102,64 @@ public class OsgiJDOPersistenceManagerFactoryBean
 
         this.bundleContext = bundleContext;
         setBeanClassLoader(Thread.currentThread().getContextClassLoader());
+    }
+
+    public String getDataSourceUrl() {
+        return dataSourceUrl;
+    }
+
+    public void setDataSourceUrl(String dataSourceUrl) {
+        this.dataSourceUrl = dataSourceUrl;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    /**
+     * Looks up a datasource from the url. The datasource can be passed either as jndi name or osgi ldap filter.
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public Object createDatasource(String url) throws Exception {
+        if (url == null) {
+            throw new Exception("Illegal datasource url format. Datasource URL cannot be null.");
+        } else if (url.trim().length() == 0) {
+            throw new Exception("Illegal datasource url format. Datasource URL cannot be empty.");
+        } else if (url.startsWith(JNDI)) {
+            String jndiName = url.substring(JNDI.length());
+            InitialContext ic = new InitialContext();
+            Object ds =  ic.lookup(jndiName);
+            return ds;
+        } else if (url.startsWith(OSGI)) {
+            String osgiFilter = url.substring(OSGI.length());
+            String clazz = null;
+            String filter = null;
+            String[] tokens = osgiFilter.split("/", 2);
+            if (tokens != null) {
+                if (tokens.length > 0) {
+                    clazz = tokens[0];
+                }
+                if (tokens.length > 1) {
+                    filter = tokens[1];
+                }
+            }
+            ServiceReference[] references = bundleContext.getServiceReferences(clazz, filter);
+            if (references != null) {
+                ServiceReference ref = references[0];
+                Object ds = bundleContext.getService(ref);
+                bundleContext.ungetService(ref);
+                return ds;
+            } else {
+                throw new Exception("Unable to find service reference for datasource: " + clazz + "/" + filter);
+            }
+        } else {
+            throw new Exception("Illegal datasource url format");
+        }
     }
 }
---
+++ kernel/common/idbus-support/src/main/java/org/atricore/idbus/kernel/common/support/jdbc/DriverManagerDataSource.java
@@ -0,0 +1,209 @@
+package org.atricore.idbus.kernel.common.support.jdbc;
+
+/**
+ * @author <a href=mailto:sgonzalez@atricore.org>Sebastian Gonzalez Oyuela</a>
+ */
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DriverManagerDataSource implements DataSource
+{
+    /** Name of the database driver. */
+    private final String driverName;
+
+    /** URL for the database. */
+    private final String url;
+
+    /** ClassLoader resolver to use for class loading */
+    private final JDBCDriverManager mgr;
+
+    /** the user name **/
+    private final String userName;
+
+    /** the password **/
+    private final String password;
+
+    /**
+     * Constructor.
+     * @param driverName Class name of the JDBC driver.
+     * @param url URL of the data source.
+     * @param mgr JDBCDriverManager to use for loading issues
+     **/
+    public DriverManagerDataSource(String driverName, String url, String userName, String password, JDBCDriverManager mgr)
+    {
+        this.driverName = driverName;
+        this.url = url;
+        this.mgr = mgr;
+        this.userName = userName;
+        this.password = password;
+
+        if (driverName != null)
+        {
+            try
+            {
+                //preferable to use ClassLoaderResolver
+                //because the driver may be loaded by another loader
+                //then the loader used by Class.forName (callerClassloader)
+                DriverDescriptor dd = mgr.getConfiguredDriver(driverName);
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Accessor for a JDBC connection for this data source.
+     * @return The connection
+     * @throws java.sql.SQLException Thrown when an error occurs obtaining the connection.
+     */
+    public Connection getConnection()
+    throws SQLException
+    {
+        return getConnection(this.userName, this.password);
+    }
+
+    /**
+     * Accessor for a JDBC connection for this data source, specifying username and password.
+     * @param userName User name for the data source (this user name is ignored)
+     * @param password Password for the data source (this password is ignored)
+     * @return The connection
+     * @throws SQLException Thrown when an error occurs obtaining the connection.
+     */
+    public Connection getConnection(String userName, String password)
+    throws SQLException
+    {
+        try
+        {
+            Properties info = new Properties();
+            if( userName != null )
+            {
+                info.put("user", this.userName);
+            }
+            if( password != null )
+            {
+                info.put("password", this.password);
+            }
+
+            return mgr.getConnection(driverName, url, info, null);
+
+        } catch (Exception e) {
+            throw new SQLException(e);
+        }
+    }
+
+    /**
+     * Accessor for the LogWriter of the driver manager.
+     * @return The Log Writer
+     */
+    public PrintWriter getLogWriter()
+    {
+        return DriverManager.getLogWriter();
+    }
+
+    /**
+     * Mutator for the LogWriter of the driver manager.
+     * @param out The Log Writer
+     */
+    public void setLogWriter(PrintWriter out)
+    {
+        DriverManager.setLogWriter(out);
+    }
+
+    /**
+     * Accessor for the Login timeout for the driver manager.
+     * @return The login timeout (seconds)
+     */
+    public int getLoginTimeout()
+    {
+        return DriverManager.getLoginTimeout();
+    }
+
+    /**
+     * Mutator for the Login timeout for the driver manager.
+     * @param seconds The login timeout (seconds)
+     */
+    public void setLoginTimeout(int seconds)
+    {
+        DriverManager.setLoginTimeout(seconds);
+    }
+
+    /**
+     * Equality operator.
+     * @param obj The object to compare against.
+     * @return Whether the objects are equal.
+     */
+    public boolean equals(Object obj)
+    {
+        if (obj == this)
+        {
+            return true;
+        }
+
+        if (!(obj instanceof DriverManagerDataSource))
+        {
+            return false;
+        }
+
+        DriverManagerDataSource dmds = (DriverManagerDataSource) obj;
+        if (driverName == null)
+        {
+            if (dmds.driverName != null)
+            {
+                return false;
+            }
+        }
+        else if (!driverName.equals(dmds.driverName))
+        {
+            return false;
+        }
+
+        if (url == null)
+        {
+            if (dmds.url != null)
+            {
+                return false;
+            }
+        }
+        else if (!url.equals(dmds.url))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Hashcode operator.
+     * @return The Hashcode for this object.
+     */
+    public int hashCode()
+    {
+        return (driverName == null ? 0 : driverName.hashCode()) ^ (url == null ? 0 : url.hashCode());
+    }
+
+    // Implementation of JDBC 4.0's Wrapper interface
+
+    public Object unwrap(Class iface) throws SQLException
+    {
+        if (!DataSource.class.equals(iface))
+        {
+            throw new SQLException("DataSource of type [" + getClass().getName() +
+                   "] can only be unwrapped as [javax.sql.DataSource], not as [" + iface.getName() + "]");
+        }
+        return this;
+    }
+
+    public boolean isWrapperFor(Class iface) throws SQLException
+    {
+        return DataSource.class.equals(iface);
+    }
+
+
+}
