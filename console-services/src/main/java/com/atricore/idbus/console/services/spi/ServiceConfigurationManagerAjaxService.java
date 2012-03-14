579e5a4e2cf02bc764f363b424d24d3129675197
Sebastian Gonzalez O <sgonzalez@atricore.com>
2012-03-14 19:27:29 -0400

    * New Service Configuration Manager

---
+++ console-services/src/main/java/com/atricore/idbus/console/services/spi/ServiceConfigurationManagerAjaxService.java
@@ -0,0 +1,13 @@
+package com.atricore.idbus.console.services.spi;
+
+/**
+ * @author <a href=mailto:sgonzalez@atricore.org>Sebastian Gonzalez Oyuela</a>
+ */
+public interface ServiceConfigurationManagerAjaxService {
+
+    // TODO : Mirror back-end methods, create DTOs (use dozer ?)
+
+    // TODO : void configureService(ServiceConfiguration cfg) throws ServiceConfigurationException;
+
+    // TODO : ServiceConfiguration lookupConfiguration(ServiceType serviceName) throws ServiceConfigurationException;
+}
