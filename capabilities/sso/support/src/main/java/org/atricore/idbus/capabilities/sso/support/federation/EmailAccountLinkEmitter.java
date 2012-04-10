f58af5557b236dcde84f660217b208c2d0a64a85
Sebastian Gonzalez O <sgonzalez@atricore.com>
2012-04-10 10:04:26 -0400

    * Email Account Linke emitter supports Subject Name IDs formatted as email * ACS Supports other Subject Name IDs than UNSPECIFIED

--- capabilities/sso/support/src/main/java/org/atricore/idbus/capabilities/sso/support/federation/EmailAccountLinkEmitter.java
+++ capabilities/sso/support/src/main/java/org/atricore/idbus/capabilities/sso/support/federation/EmailAccountLinkEmitter.java
@@ -24,10 +24,7 @@ package org.atricore.idbus.capabilities.sso.support.federation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.atricore.idbus.capabilities.sso.support.core.NameIDFormat;
-import org.atricore.idbus.kernel.main.federation.AccountLink;
-import org.atricore.idbus.kernel.main.federation.AccountLinkEmitter;
-import org.atricore.idbus.kernel.main.federation.DynamicAccountLinkImpl;
-import org.atricore.idbus.kernel.main.federation.SubjectAttribute;
+import org.atricore.idbus.kernel.main.federation.*;
 
 import javax.security.auth.Subject;
 import java.util.Set;
@@ -44,10 +41,26 @@ public class EmailAccountLinkEmitter implements AccountLinkEmitter {
 
     public AccountLink emit ( Subject subject ) {
 
+        // If subjectName ID  is email formatted, use it
+        Set<SubjectNameID> nameIds = subject.getPrincipals(SubjectNameID.class);
+        if (nameIds != null) {
+            if ( logger.isDebugEnabled() )
+                logger.debug( "SubjectNameID Pricipals found: " + nameIds.size() );
+
+            for (SubjectNameID nameId : nameIds) {
+                if (nameId.getFormat() == null || nameId.getFormat().equals(NameIDFormat.EMAIL.getValue())) {
+                    String email = nameId.getName();
+                    return new DynamicAccountLinkImpl(subject, email.substring(0, email.indexOf("@")), NameIDFormat.UNSPECIFIED.getValue());
+                }
+            }
+        }
+
+
+        // Look for subject attributes that may be an email (TODO : make configurable)
         Set<SubjectAttribute> subjectAttrs = subject.getPrincipals( SubjectAttribute.class );
 
         if ( logger.isDebugEnabled() )
-            logger.debug( "Pricipals found: " + subjectAttrs.size() );
+            logger.debug( "SubjectAttribute Pricipals found: " + subjectAttrs.size() );
 
         for (SubjectAttribute subjectAttribute : subjectAttrs ) {
 
