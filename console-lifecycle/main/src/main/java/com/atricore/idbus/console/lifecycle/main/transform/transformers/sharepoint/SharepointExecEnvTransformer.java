22870808bb856ead09f969a4233765cf8c5bfe57
sgonzalez <sgonzalez@atricore.com>
2013-04-18 01:46:29 -0400

    * Removed unused class

--- console-lifecycle/main/src/main/java/com/atricore/idbus/console/lifecycle/main/transform/transformers/sharepoint/SharepointExecEnvTransformer.java
+++
@@ -1,8 +0,0 @@
-package com.atricore.idbus.console.lifecycle.main.transform.transformers.sharepoint;
-
-/**
- * @author: sgonzalez@atriocore.com
- * @date: 4/17/13
- */
-public class SharepointExecEnvTransformer {
-}
