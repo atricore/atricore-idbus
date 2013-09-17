7df5b2f074cab23923ba53454c157a00995fa78a
sgonzalez <sgonzalez@atricore.com>
2013-09-17 17:12:07 -0400

    * NEW : DotNet OAuth2 client distribution (basic API so far)

--- capabilities/oauth2/clients/oauth2-client-dotnet/Client/OAuth2Client.cs
+++ capabilities/oauth2/clients/oauth2-client-dotnet/Client/OAuth2Client.cs
@@ -11,38 +11,71 @@ using Atricore.OAuth2Protocol;
 
 namespace Atricore.OAuth2Client
 {
-    class OAuth2Client
+    public class OAuth2Client
     {
+
+        private OAuth2Protocol.AccessTokenRequestor.OAuthPortTypeClient client;
+
+        private String clientId;
+
+        private String clientSecret;
+
         static void Main(string[] args)
         {
-                string clientId = ConfigurationManager.AppSettings["clientId"];
-                string clientSecret = ConfigurationManager.AppSettings["clientSecret"];
-                string username = ConfigurationManager.AppSettings["username"];
-                string password = ConfigurationManager.AppSettings["password"];
-                string authorizationServerEndpoint = ConfigurationManager.AppSettings["authorizationServerEndpoint"];
-                string serviceProviderAlias = ConfigurationManager.AppSettings["serviceProviderAlias"];
-                string resourceServerEndpoint = ConfigurationManager.AppSettings["resourceServerEndpoint"];
+            string username = ConfigurationManager.AppSettings["username"];
+            string password = ConfigurationManager.AppSettings["password"];
 
-                var messageElement = new TextMessageEncodingBindingElement();
-                var transportElement = new HttpTransportBindingElement();
-                messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap11, AddressingVersion.None);
-                CustomBinding binding = new CustomBinding(messageElement, transportElement);
-                EndpointAddress endpoint = new EndpointAddress(new Uri(authorizationServerEndpoint));
+            Atricore.OAuth2Client.OAuth2Client oauth2Client = new Atricore.OAuth2Client.OAuth2Client();
+            oauth2Client.init();
+            String accessToken = oauth2Client.requestToken(username, password);
 
-                OAuth2Protocol.AccessTokenRequestor.OAuthPortTypeClient client = new OAuth2Protocol.AccessTokenRequestor.OAuthPortTypeClient(binding, endpoint);
-
-                OAuth2Protocol.AccessTokenRequestor.AccessTokenRequestType atreq = new OAuth2Protocol.AccessTokenRequestor.AccessTokenRequestType();
-                atreq.clientId = clientId;
-                atreq.clientSecret = clientSecret;
-                atreq.username = username;
-                atreq.password = password;
-                OAuth2Protocol.AccessTokenRequestor.AccessTokenResponseType atrsp = new OAuth2Protocol.AccessTokenRequestor.AccessTokenResponseType();
-                atrsp = client.AccessTokenRequest(atreq);
-                String accessToken = atrsp.accessToken;
-
-                string url = string.Format("{0}?atricore_sp_alias={1}&atricore_security_token={2}", resourceServerEndpoint, serviceProviderAlias, Uri.EscapeDataString(accessToken));
-                
-                Console.WriteLine(url);
+            string resourceServerEndpoint = ConfigurationManager.AppSettings["resourceServerEndpoint"];
+            string serviceProviderAlias = ConfigurationManager.AppSettings["serviceProviderAlias"];
+            string url = string.Format("{0}?atricore_sp_alias={1}&atricore_security_token={2}", resourceServerEndpoint, serviceProviderAlias, Uri.EscapeDataString(accessToken));
+            Console.WriteLine(url);
         }
+
+        public void init()
+        {
+            string authorizationServerEndpoint = ConfigurationManager.AppSettings["authorizationServerEndpoint"];
+            clientId = ConfigurationManager.AppSettings["clientId"];
+            clientSecret = ConfigurationManager.AppSettings["clientSecret"];
+
+            var messageElement = new TextMessageEncodingBindingElement();
+            var transportElement = new HttpTransportBindingElement();
+            messageElement.MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap11, AddressingVersion.None);
+            CustomBinding binding = new CustomBinding(messageElement, transportElement);
+            EndpointAddress endpoint = new EndpointAddress(new Uri(authorizationServerEndpoint));
+
+            client = new OAuth2Protocol.AccessTokenRequestor.OAuthPortTypeClient(binding, endpoint);
+        }
+
+        public String requestToken(String usr, String pwd) 
+        {
+            OAuth2Protocol.AccessTokenRequestor.AccessTokenRequestType atreq = new OAuth2Protocol.AccessTokenRequestor.AccessTokenRequestType();
+            atreq.clientId = clientId;
+            atreq.clientSecret = clientSecret;
+            atreq.username = usr;
+            atreq.password = pwd;
+            OAuth2Protocol.AccessTokenRequestor.AccessTokenResponseType atrsp = new OAuth2Protocol.AccessTokenRequestor.AccessTokenResponseType();
+            atrsp = client.AccessTokenRequest(atreq);
+            String accessToken = atrsp.accessToken;
+
+            if (accessToken == null)
+                throw new OAuth2ClientException(atrsp.error + " : " + atrsp.error_description);
+
+            return accessToken;
+        }
+    }
+
+    [Serializable()]
+    public class OAuth2ClientException : System.Exception
+    {
+        public OAuth2ClientException () : base() { }
+        public OAuth2ClientException (string message) : base(message) { }
+        public OAuth2ClientException (string message, System.Exception inner) : base(message, inner) { }
+
+        protected OAuth2ClientException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
     }
 }
