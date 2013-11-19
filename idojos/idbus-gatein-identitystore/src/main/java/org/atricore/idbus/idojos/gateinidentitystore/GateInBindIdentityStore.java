4714da0f5885527af0c58de39e72aa8807cc3973
sgonzalez <sgonzalez@atricore.com>
2013-11-18 19:07:00 -0500

    * NEW : Gatein Id Store uses Default HTTP client (provides performance improvements)

--- idojos/idbus-gatein-identitystore/src/main/java/org/atricore/idbus/idojos/gateinidentitystore/GateInBindIdentityStore.java
+++ idojos/idbus-gatein-identitystore/src/main/java/org/atricore/idbus/idojos/gateinidentitystore/GateInBindIdentityStore.java
@@ -23,11 +23,13 @@
 package org.atricore.idbus.idojos.gateinidentitystore;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.util.URIUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.atricore.idbus.kernel.main.authn.Credential;
 import org.atricore.idbus.kernel.main.authn.CredentialKey;
 import org.atricore.idbus.kernel.main.authn.CredentialProvider;
@@ -36,6 +38,11 @@ import org.atricore.idbus.kernel.main.authn.scheme.AuthenticationScheme;
 import org.atricore.idbus.kernel.main.store.exceptions.SSOIdentityException;
 import org.atricore.idbus.kernel.main.store.identity.BindContext;
 import org.atricore.idbus.kernel.main.store.identity.BindableCredentialStore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
 
 
 /**
@@ -91,11 +98,17 @@ public class GateInBindIdentityStore implements BindableCredentialStore {
             log.debug("Username: " + username);
             log.debug("Password: " + password);
 
+            /*
             String authnPath = URIUtil.encodePath(
-                            (!gateInContext.isEmpty() ? "/" + gateInContext : "") +
+                    (!gateInContext.isEmpty() ? "/" + gateInContext : "") +
                             "/rest/sso/authcallback/auth/" + new String(Base64.encodeBase64(username.getBytes())) +
-                            "/"  + new String(Base64.encodeBase64(password.getBytes()))
-                          );
+                            "/" + new String(Base64.encodeBase64(password.getBytes()))
+            );
+            */
+            String authnPath =
+                    (!gateInContext.isEmpty() ? "/" + gateInContext : "") +
+                            "/rest/sso/authcallback/auth/" + new String(Base64.encodeBase64(username.getBytes())) +
+                            "/" + new String(Base64.encodeBase64(password.getBytes()));
             StringBuilder urlBuffer = new StringBuilder();
 
             urlBuffer.append("http://" + gateInHost + ":" + gateInPort + authnPath);
@@ -110,31 +123,112 @@ public class GateInBindIdentityStore implements BindableCredentialStore {
 
     //------------------------------------------------------------------------------------------------------------------------------------------
     private boolean executeRemoteCall(String authUrl) throws Exception {
-        HttpClient client = new HttpClient();
-        GetMethod method = null;
+        if (log.isDebugEnabled())
+            log.debug("Starting remote call using HTTP client\n["+authUrl+"]");
+
+        InputStream instream = null;
+        HttpRequestBase httpReq = null;
+
         try {
-            method = new GetMethod(authUrl);
 
-            int status = client.executeMethod(method);
-            String response = method.getResponseBodyAsString();
+            DefaultHttpClient httpClient = new DefaultHttpClient();
 
-            switch (status) {
-                case 200:
-                    if (response.equals(Boolean.TRUE.toString())) {
-                        return true;
-                    }
-                    break;
+            if (log.isTraceEnabled())
+                log.trace("Client OK");
+
+            httpReq = new HttpGet(authUrl);
+
+            if (log.isTraceEnabled())
+                log.trace("Method OK");
+
+            HttpResponse httpRes = httpClient.execute(httpReq);
+            int status = httpRes.getStatusLine().getStatusCode();
+
+            if (log.isTraceEnabled())
+                log.trace("Client execution OK");
+
+            if (log.isTraceEnabled())
+                log.trace("Response OK");
+
+            // Get hold of the response entity
+            HttpEntity entity = httpRes.getEntity();
+
+
+            if (entity != null) {
+
+                instream = entity.getContent();
+
+                // If the response does not enclose an entity, there is no need
+                byte[] buff = new byte[1024];
+                ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
+                String response = null;
+
+
+                // Just ignore the content ...
+                // should we do something with this ?!
+                int r = instream.read(buff);
+                int total = r;
+                while (r > 0) {
+                    baos.write(buff, 0, r);
+                    r = instream.read(buff);
+                    total += r;
+                }
+
+                response = baos.toString();
+
+                if (total > 0)
+                    log.debug("Read response content size : " + total + " [" + response + "] ");
+
+                switch (status) {
+                    case 200:
+                        if (response != null && Boolean.parseBoolean(response)) {
+                            return true;
+                        }
+                        break;
+                    default:
+                        log.warn("Received invalid HTTP status " + status + " for " + authUrl);
+                }
+
+                return false;
+
+            } else {
+
+                // Not authenticated
+                log.warn("No response body received for " + authUrl);
+                return false;
             }
 
-            return false;
+
+        } catch (IOException ex) {
+            // In case of an IOException the connection will be released
+            // back to the connection manager automatically
+            throw ex;
+        } catch (RuntimeException ex) {
+            // In case of an unexpected exception you may want to abort
+            // the HTTP request in order to shut down the underlying
+            // connection immediately.
+            if (httpReq != null) {
+                try {
+                    httpReq.abort();
+                } catch(Exception e) {
+                    log.error(e.getMessage(), e);
+
+                }
+            }
         } finally {
-            if (method != null) {
-                method.releaseConnection();
+            // Closing the input stream will trigger connection release
+            try {
+                instream.close();
+            } catch (Exception ignore) {
+                // Ignore this ...
             }
         }
+
+        return false;
     }
 
-    public Credential[] loadCredentials(CredentialKey key, CredentialProvider cp) throws SSOIdentityException {
+    public Credential[] loadCredentials (CredentialKey key, CredentialProvider cp)throws SSOIdentityException {
         throw new UnsupportedOperationException("GateIn credentials cannot be accessed through this mechanism");
     }
+
 }
