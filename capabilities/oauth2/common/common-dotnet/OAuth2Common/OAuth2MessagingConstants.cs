b0da6f0b59633f277434c8526e559fc1adefddb2
sgonzalez <sgonzalez@atricore.com>
2015-03-19 21:38:19 -0400

    * NEW : OAuth2 resource server (dotnet)

---
+++ capabilities/oauth2/common/common-dotnet/OAuth2Common/OAuth2MessagingConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace OAuth2Common
+{
+    public class OAuth2MessagingConstants
+    {
+
+        static String SERVICE_NAME = "urn:org:atricore:idbus:common:oauth:2.0:wsdl:OAuthService";
+
+        static String PORT_NAME = "urn:org:atricore:idbus:common:oauth:2.0:wsdl:soap";
+    }
+}
