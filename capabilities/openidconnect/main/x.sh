dbdd72457f6cb449725f0affe9cc73e365e261fa
sgonzalez <sgonzalez@atricore.com>
2020-09-09 12:06:34 -0400

    * UPD : SSHD Server to support RSA keys

---
+++ capabilities/openidconnect/main/x.sh
@@ -0,0 +1,2 @@
+tar zcvf /tmp/x.tgz   src/main/java/org/atricore/idbus/capabilities/openidconnect/main/op/emitter/IDTokenEmitter.java src/main/java/org/atricore/idbus/capabilities/openidconnect/main/op/producers/TokenProducer.java  src/main/java/org/atricore/idbus/capabilities/openidconnect/main/rp/producer/TokenProducer.java
+
