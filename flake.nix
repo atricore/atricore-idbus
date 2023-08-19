d02f8df1e58154bc3ca43e0078a83cd345748335
sgonzalez <sgonzalezoyuela@gmail.com>
2023-08-19 16:17:46 -0300

    Configure env using nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,26 @@
 {
-  description = "A Nix-flake-based Java development environment";
+  description = "A Nix-flake-based Java 8 + Maven 3.5.4 dev env";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
     let
-      javaVersion = 8; # Change this value to update the whole stack
+      javaVersion = 8;
       overlays = [
-        (final: prev: rec {
-          jdk = prev."jdk${toString javaVersion}";
-          maven = prev.maven.override { inherit jdk; };
+        (final: prev: {
+          jdk = prev."jdk${toString javaVersion}_headless";
+          maven = prev.stdenv.mkDerivation rec {
+            name = "maven-3.5.4";
+            src = prev.fetchurl {
+              url = "https://archive.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz";
+              sha256 = "sha256-zlCxyRNky3fv43dvdWptkrdtkDiwoHgvfVOs8emXoU0=";
+            };
+            buildInputs = [ prev.maven ];
+            buildCommand = ''
+              tar -xf $src
+              mv apache-maven-3.5.4 $out
+            '';
+          };
         })
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -20,7 +31,12 @@
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ gradle jdk maven ];
+          packages = with pkgs; [ jdk maven ];
+          shellHook = ''
+            export JAVA_HOME="${pkgs.jdk}/lib/openjdk"
+            export MAVEN_HOME="${pkgs.maven}"
+            export MAVEN_OPTS="-Dmaven.test.skip=true -Xmx2048m"
+          '';
         };
       });
     };
